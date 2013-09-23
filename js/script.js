4ccdeee03213c8d2ad38b54550adb7a6b196cb1a
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-09-22 23:13:58 -0400

    current iteration of DCenter theme

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-/*
-
-*/
-
-(function ($, Drupal, window, document, undefined) {
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+ 
+ (function ($, Drupal, window, document, undefined) {
 
 // Place your code here.
+  //hide excess <p> on project pages...
+  $(document).ready(function() {
+    $('.page-projects .project .body').each(function() {
+        $('p').hide();
+        $('p:first-child').show();
+      });
+    });
 
-  $(document).ready(function(){
-    $("#test").sticky({topSpacing:0});
-  });
+  $(document).ready(function() {
+    $('.page-activities .project .body').each(function() {
+        $('p').hide();
+        $('p:first-child').show();
+      });
+    });
+
+// End OOKB JS
 
 })(jQuery, Drupal, this, this.document);
